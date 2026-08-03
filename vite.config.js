28d048056b6bb46f339d40916fd458cada84e0b5
pealbert <pealbert@email.cz>
2026-08-03 18:25:50 +0200

    Add privacy policy page, complete info and stylize it

--- vite.config.js
+++ vite.config.js
@@ -7,20 +7,21 @@ export default defineConfig({
 			name: 'flatten-html-output',
 			enforce: 'post',
 			generateBundle(_, bundle) {
-			for (const [fileName, file] of Object.entries(bundle)) {
-				if (fileName.startsWith('pages/') && fileName.endsWith('.html')) {
-					file.fileName = fileName.replace('pages/', '');
+				for (const [fileName, file] of Object.entries(bundle)) {
+					if (fileName.startsWith('pages/') && fileName.endsWith('.html')) {
+						file.fileName = fileName.replace('pages/', '');
+					}
 				}
-			}
 			},
 		},
 	],
 	build: {
 		rollupOptions: {
 			input: {
-			main: resolve(import.meta.dirname, 'index.html'),
-			credits: resolve(import.meta.dirname, 'pages/credits.html'),
-			registration: resolve(import.meta.dirname, 'pages/registration.html'),
+				main: resolve(import.meta.dirname, 'index.html'),
+				registration: resolve(import.meta.dirname, 'pages/registration.html'),
+				policy: resolve(import.meta.dirname, 'pages/policy.html'),
+				credits: resolve(import.meta.dirname, 'pages/credits.html'),
 			},
 		},
 	},
