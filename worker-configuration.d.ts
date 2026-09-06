a7424e0ac2d29be3b33af6cccb9409ccc9b42d84
pealbert <pealbert@email.cz>
2026-09-06 18:50:44 +0200

    fix: change footer to nested css

--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -492,7 +492,7 @@ declare const performance: Performance;
 declare const Cloudflare: Cloudflare;
 declare const origin: string;
 declare const navigator: Navigator;
-interface TestController {}
+type TestController = {};
 interface ExecutionContext<Props = unknown> {
 	waitUntil(promise: Promise<any>): void;
 	passThroughOnException(): void;
@@ -680,9 +680,9 @@ interface DurableObjectNamespaceGetDurableObjectOptions {
 	locationHint?: DurableObjectLocationHint;
 	routingMode?: DurableObjectRoutingMode;
 }
-interface DurableObjectClass<
+type DurableObjectClass<
 	_T extends Rpc.DurableObjectBranded | undefined = undefined,
-> {}
+> = {};
 interface DurableObjectState<Props = unknown> {
 	waitUntil(promise: Promise<any>): void;
 	readonly exports: Cloudflare.Exports;
@@ -3273,8 +3273,8 @@ interface TraceItem {
 interface TraceItemAlarmEventInfo {
 	readonly scheduledTime: Date;
 }
-interface TraceItemConnectEventInfo {}
-interface TraceItemCustomEventInfo {}
+type TraceItemConnectEventInfo = {};
+type TraceItemCustomEventInfo = {};
 interface TraceItemScheduledEventInfo {
 	readonly scheduledTime: number;
 	readonly cron: string;
@@ -13633,7 +13633,7 @@ declare abstract class D1PreparedStatement {
 // but this will ensure type checking on older versions still passes.
 // TypeScript's interface merging will ensure our empty interface is effectively
 // ignored when `Disposable` is included in the standard lib.
-interface Disposable {}
+type Disposable = {};
 /**
  * The returned data after sending an email
  */
@@ -13778,6 +13778,7 @@ declare module "cloudflare:email" {
 		prototype: EmailMessage;
 		new (from: string, to: string, raw: ReadableStream | string): EmailMessage;
 	};
+
 	export { _EmailMessage as EmailMessage };
 }
 /**
@@ -14671,7 +14672,7 @@ declare namespace Cloudflare {
 	// will merge all declarations.
 	//
 	// You can use `wrangler types` to generate the `Env` type automatically.
-	interface Env {}
+	type Env = {};
 	// Project-specific parameters used to inform types.
 	//
 	// This interface is, again, intended to be declared in project-specific files, and then that
@@ -14690,7 +14691,7 @@ declare namespace Cloudflare {
 	//     }
 	//
 	// You can use `wrangler types` to generate `GlobalProps` automatically.
-	interface GlobalProps {}
+	type GlobalProps = {};
 	// Evaluates to the type of a property in GlobalProps, defaulting to `Default` if it is not
 	// present.
 	type GlobalProp<K extends string, Default> = K extends keyof GlobalProps
@@ -14937,6 +14938,7 @@ declare module "cloudflare:sockets" {
 		address: string | SocketAddress,
 		options?: SocketOptions,
 	): Socket;
+
 	export { _connect as connect };
 }
 /**
